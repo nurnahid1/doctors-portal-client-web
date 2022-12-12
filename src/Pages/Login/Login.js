7c167291b92fd751b02d27be51c467583b808b42
nahid770 <nurnahid10@gmail.com>
2022-12-12 12:56:24 +0600

    login form started

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import {auth} from '../../firebase.init';
 
 const Login = () => {
+ 
+    // const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user)
+    }
     return (
-        <div>
-            <h2>login page</h2>
+        <div className='h-screen flex justify-center items-center'>
+            <div className="card w-96  bg-base-100 shadow-xl">
+            <div className="card-body ">
+                <h2 className=" text-center text-xl">Login</h2>
+                
+                <div className='divider'>or</div>
+                <div className="card-actions justify-center">
+                {/* <button onClick={()=>signInWithGoogle()} className="btn btn-outline text-center">Google sign in</button> */}
+
+                <button className='border' onClick={()=> signInWithGoogle()}>google sign in</button>
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,3 +1,30 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:process.env.REACT_APP_apiKey,
+//   authDomain:process.env.REACT_APP_authDomain,
+//   projectId:process.env.REACT_APP_projectId,
+//   storageBucket:process.env.REACT_APP_storageBucket,
+//   messagingSenderId:process.env.REACT_APP_messagingSenderId,
+//   appId:process.env.REACT_APP_appId,
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+
+// export default auth;
+
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -6,16 +33,22 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  // apiKey: "AIzaSyBcPvheBFxa8Gz4bEs71A3qdRkhuSYwyNU",
+  // authDomain: "doctors-portal-client-web.firebaseapp.com",
+  // projectId: "doctors-portal-client-web",
+  // storageBucket: "doctors-portal-client-web.appspot.com",
+  // messagingSenderId: "92621647077",
+  // appId: "1:92621647077:web:865d5e03f4c91b8717780c"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
-const auth = getAuth(app);
-export default auth;
+export default app;
